40b5c19b7bcd03fde2406861b3413559dc66ef71
Gabrielle <dev.gabriellejonsson@gmail.com>
2021-04-13 21:03:33 +0200

    Added responsive footer for mobile and desktop

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,17 +1,133 @@
 <script>
+    import FaInstagram from 'svelte-icons/fa/FaInstagram.svelte'
+    import FaBehance from 'svelte-icons/fa/FaBehance.svelte'
+    import FaFacebookF from 'svelte-icons/fa/FaFacebookF.svelte'
 
+    let currentYear = new Date().getFullYear();
 </script>
 
-<footer class="footer">
+<footer>
+    <div class="footer-mobile">
+        <div class="footer-mobile__icons">
+            <div class="footer-mobile__icons-icon">
+                <FaFacebookF />
+            </div>
+            <div class="footer-mobile__icons-icon">
+                <FaBehance />
+            </div>
+            <a href="https://instagram.com/glimfotografi">
+                <div class="footer-mobile__icons-icon">
+                    <FaInstagram />
+                </div>
+            </a>
+        </div>
+        <div class="footer-mobile__copyright-container">
+            <span class="footer__copyright">jonssons.io © {currentYear}</span>
+        </div>
+    </div>
+    <div class="footer-desktop">
+        <div class="footer-desktop__icons">
+            <a href="#">
+                <div class="footer-desktop__icons-icon">
+                    <FaFacebookF />
+                </div>
+            </a>
+            <a href="#">
+                <div class="footer-desktop__icons-icon">
+                    <FaBehance />
+                </div>
+            </a>
+            <a href="https://instagram.com/glimfotografi">
+                <div class="footer-desktop__icons-icon">
+                    <FaInstagram />
+                </div>
+            </a>
+        </div>
+        <div class="footer-desktop__copyright-container">
+            <span class="footer__copyright">jonssons.io © {currentYear}</span>
+        </div>
+    </div>
 
 </footer>
 
 <style>
-footer {
-    position: sticky;
-    bottom: 0px;
-    width: 100vw;
-    height: 100px;
-    background: #F0F0F0;
-}
+    footer {
+        position: absolute;
+        bottom: 0px;
+        width: 100vw;
+        height: 100px;
+        background: #F0F0F0;
+        display: flex;
+        flex-flow: row nowrap;
+        align-items: center;
+        justify-content: center;
+    }
+    .footer-mobile {
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
+        align-items: center;
+        margin: 0.8em 0;
+    }
+    .footer-mobile__icons {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: center;
+        align-items: center;
+    }
+    .footer-mobile__icons-icon {
+        height: 36px;
+        width: 36px;
+        margin: 0 1em;
+        display: flex;
+        flex-flow: row nowrap;
+        align-items: center;
+        color: black;
+    }
+    .footer-mobile__copyright-container {
+        margin-top: 0.8em;
+        text-align: center;
+    }
+    .footer__copyright {
+        font-family: 'Exo', sans-serif;
+        font-weight: 500;
+    }
+    .footer-desktop {
+        display: none;
+    }
+
+    @media screen and (min-width: 600px) {
+        .footer-mobile {
+            display: none;
+        }
+        .footer-desktop {
+            margin: 2em;
+            width: 100vw;
+            display: flex;
+            flex-flow: column nowrap;
+        }
+        .footer-desktop__icons {
+            display: flex;
+            flex-flow: row nowrap;
+            justify-content: flex-end;
+        }
+        .footer-desktop__icons-icon {
+            margin: 0 1em;
+            height: 36px;
+            width: 36px;
+            color: black;
+            display: flex;
+            align-items: center;
+        }
+        .footer-desktop__copyright-container {
+            margin-top: 0.5em;
+            margin-right: 1em;
+            text-align: right;
+        }
+    }
+    @media screen and (min-width: 1264px) {
+        .footer-desktop {
+            margin-right: 6em;
+        }
+    }
 </style>
